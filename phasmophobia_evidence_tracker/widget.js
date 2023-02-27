188f7d128dc63aea1ed27f47e8cddeb827c01b22
billyjbryant <3013565+billyjbryant@users.noreply.github.com>
2023-02-27 10:47:42 -0800

    Updating Version & Readme

--- phasmophobia_evidence_tracker/widget.js
+++ phasmophobia_evidence_tracker/widget.js
@@ -1,6 +1,6 @@
 /** @format */
 
-const version = '4.2.1 (Apocalypse)';
+const version = '4.2.2 (Tempest)';
 
 // Order is important here:
 // EMF-5 | Freezing | Spirit Box | Writing | Orbs | Fingerprints | DOTS
